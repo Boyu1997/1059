890f397a1e3dfc9a7484913ffd3be9669368de0a
Boyu1997 <jiangby97@gmail.com>
2019-07-21 17:37:52 -0400

    chore: move react app to client folder

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>1059</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
